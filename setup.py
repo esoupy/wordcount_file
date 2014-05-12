d1b6a6868d3d0afcc689ef820187b81a37008360
Eric Sales <eric@soupynet.com>
2014-05-11 23:50:39 -0700

    distribution for wordcount_file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wordcount_file',
+      version='0.1',
+      description='Counts the words in an ASCII file.',
+      url='http://github.com/esoupy/wordcount_file',
+      author='Eric Sales',
+      author_email='eric@soupynet.com',
+      license='MIT',
+      packages=['wordcount_file'],
+      zip_safe=False)
---
+++ wordcount_file/wordcount.py
@@ -0,0 +1,96 @@
+
+def count_words(word_list, word_dict={}, case_sensitive=False):
+    """
+    Count the words in a list and merge with word_dict if included
+    return word_dict
+    """
+    for word in word_list:
+        # change case and remove any punctuations at the start/end of word #
+        if word:
+            # if not case sensitive change words to lower case
+            if not case_sensitive:
+                w = word.lower()
+            # strip any leading dos carriage returns and line feeds
+            while w.endswith('\n') or w.endswith('\r'):
+                w = w.rstrip('\n')
+                w = w.rstrip('\r')
+            # move to next word in list if nothing to process
+            if not w:
+                continue
+
+            # check single char items
+            if len(w) == 1:
+                if not w.isalnum():
+                    # skip any non alphanumeric single chars
+                    continue
+            else:
+                # strip any non alphanumeric first and last chars
+                if not w[0].isalnum():
+                    w = w[1:]
+                if not w[-1].isalnum():
+                    w = w[:-1]
+
+            # Increment word count or add to dictionary
+            if w in word_dict.keys():
+                word_dict[w] += 1
+            else:
+                word_dict[w] = 1
+
+    return word_dict
+
+
+class WordCount:
+    """
+    Count the words in an ascii text file
+    """
+
+    def __init__(self, textfile, case_sensitive=False):
+        """
+        initialize with textfile
+        """
+        from collections import OrderedDict
+
+        self.words = {}
+        self.counts = {}
+        self.textfile = textfile
+        SEPARATORS = [' ',';',':','=',',','(',')','{','}','[',']','|','<','>']
+        with open(textfile, 'r') as f:
+            for line in f:
+                _words_in_line = [line]
+                # split words by separators #
+                for sep in SEPARATORS:
+                    line, _words_in_line = _words_in_line, []
+                    for seq in line:
+                        _words_in_line += seq.split(sep)
+                #_words_in_line = line.split(' ')
+
+                if _words_in_line:
+                    self.words = count_words(_words_in_line, self.words, case_sensitive)
+
+        # create counts dictionary for faster count sorting #
+        for _word in self.words:
+            count = self.words[_word]
+            if count in self.counts:
+                self.counts[count].append(_word)
+            else:
+                self.counts[count] = [ _word ]
+        self.counts = OrderedDict(sorted(self.counts.items(), reverse=True))
+
+        self.totalwords = len(self.words.keys())
+
+    def __repr__(self):
+        return "{ 'textfile': '%s', 'totalwords': %s }" % (self.textfile, self.totalwords)
+
+    def show_topwords(self, num=5):
+        """
+        print the topwords (default = top 5 words)
+        """
+        count = 0
+        for _count in self.counts:
+            if count < num:
+                print "%i => %s" % (_count, self.counts[_count])
+                count += 1
+            else:
+                return
+
+
